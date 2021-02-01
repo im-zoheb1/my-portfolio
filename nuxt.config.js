63ae18d0c7e7cc82f63eb5633e980d0e8be72b8e
Zuhaib Khan <xuhaib002@gmail.com>
2021-02-01 13:28:25 +0500

    changing the title and title icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,14 +2,14 @@ export default {
   target: 'static',
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'portfolio',
+    title: 'Zohaib Khan',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/logo.ico' },
       {
         rel: 'stylesheet',
         href:
