58a294acf144e41383b58d3589ab4511be793a2b
xuhaib002@gmail.com <xuhaib002@gmail.com>
2021-08-13 10:53:36 +0500

    updated portfolio skills

--- components/Home/Skills.vue
+++ components/Home/Skills.vue
@@ -3,95 +3,49 @@
     <div class="container">
       <div class="skills__content">
         <div data-aos="fade-up" class="skills__title">
-          <img
-            class="skills__title--icon"
-            src="~assets/svg/ninja.svg"
-            alt="skills"
-          />
-          <div class="skills__title--heading heading--primary">
-            I'm Ninja at
-          </div>
+          <img class="skills__title--icon" src="~assets/svg/ninja.svg" alt="skills" />
+          <div class="skills__title--heading heading--primary">I'm Ninja at</div>
         </div>
         <div class="skills__cards">
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/html.svg"
-              alt="html"
-            />
+            <img class="skills__card--icon" src="~assets/svg/html.svg" alt="html" />
             <div class="skills__card--title">html</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/css.svg"
-              alt="css"
-            />
+            <img class="skills__card--icon" src="~assets/svg/css.svg" alt="css" />
             <div class="skills__card--title">css</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/javascript.svg"
-              alt="javascript"
-            />
+            <img class="skills__card--icon" src="~assets/svg/javascript.svg" alt="javascript" />
             <div class="skills__card--title">javascript</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/vue.svg"
-              alt="vuejs"
-            />
+            <img class="skills__card--icon" src="~assets/svg/vue.svg" alt="vuejs" />
             <div class="skills__card--title">vuejs</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/nuxt.svg"
-              alt="nuxtjs"
-            />
+            <img class="skills__card--icon" src="~assets/svg/nuxt.svg" alt="nuxtjs" />
             <div class="skills__card--title">nuxtjs</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/sass.svg"
-              alt="sass"
-            />
+            <img class="skills__card--icon" src="~assets/svg/react.svg" alt="bootstrap" />
+            <div class="skills__card--title">react</div>
+          </div>
+          <div data-aos="fade-up" class="skills__card">
+            <img class="skills__card--icon" src="~assets/svg/typescript.svg" alt="github" />
+            <div class="skills__card--title">typescript</div>
+          </div>
+          <div data-aos="fade-up" class="skills__card">
+            <img class="skills__card--icon" src="~assets/svg/sass.svg" alt="sass" />
             <div class="skills__card--title">sass</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/github.svg"
-              alt="github"
-            />
-            <div class="skills__card--title">github</div>
+            <img class="skills__card--icon" src="~assets/svg/bootstrap.svg" alt="npm" />
+            <div class="skills__card--title">Bootstrap</div>
           </div>
           <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/bootstrap.svg"
-              alt="bootstrap"
-            />
-            <div class="skills__card--title">bootstrap</div>
-          </div>
-          <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/figma.svg"
-              alt="figma"
-            />
+            <img class="skills__card--icon" src="~assets/svg/figma.svg" alt="figma" />
             <div class="skills__card--title">figma</div>
-          </div>
-          <div data-aos="fade-up" class="skills__card">
-            <img
-              class="skills__card--icon"
-              src="~assets/svg/npm.svg"
-              alt="npm"
-            />
-            <div class="skills__card--title">npm</div>
           </div>
         </div>
       </div>
